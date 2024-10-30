b81f73e023b901fcfc07574aa3112ec63ecebca7
Damian X <dx4336969@gmail.com>
2024-10-30 09:02:56 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const fs = require('fs');
+dotenv.config({path:'./config.env'})
+const Cities = JSON.parse(fs.readFileSync(`${__dirname}/data/cities.json`))
+const app = express()
+
+app.get('/api/v1/cities',(req,res,next)=>{
+    res.status(200).json({
+        status:'success',
+        data:{
+            Cities
+        }
+    })
+    next()
+})
+
+const port = process.env.PORT || 3000
+app.listen(port, ()=>{
+    console.log(`App running on port ${port}`)
+})
